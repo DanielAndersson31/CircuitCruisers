60de877bac61f1658413aaaf8551a5f00ea8c618
Daniel <daniel.andersson311@gmail.com>
2023-06-21 07:57:24 +0200

    Updated Index Account page to display more information about the currently signed in User

--- CircuitCruisers/Controllers/AccountController.cs
+++ CircuitCruisers/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CircuitCruisers.Models.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CircuitCruisers.Controllers
 {
@@ -7,12 +10,25 @@ namespace CircuitCruisers.Controllers
 
     public class AccountController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
-        {
-            var viewModel = new 
 
-            return View();
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.User.Identity != null!)
+            {
+                var appUser = await _userManager.Users.FirstOrDefaultAsync(x => x.Email == HttpContext.User.Identity.Name);
+
+                return View(appUser);
+            }
+
+           return RedirectToAction("index", "login");
         }
     }
 }
